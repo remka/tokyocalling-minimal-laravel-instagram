243b62ccb35365a9f1ea6b68e96be923c2ac9332
Remka <remuka@gmail.com>
2017-04-25 03:34:19 +0900

    Small changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,11 +5,22 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Http\Controllers\Controller;
 use Vinkla\Instagram\Instagram;
+use \Cache;
 
 class HomeController extends Controller {
   public function listInstagram() {
-    $instagram = new Instagram();
+    if (Cache::has('cache_instagram')) {
+      //echo('has cache');
+      $results = Cache::get('cache_instagram');
+    } else {
+      //echo('has NO cache. Create!');
+      $instagram = new Instagram();
+      $results = $instagram->get('_tokyocalling');
+      //Cache::put('cache_instagram', $results, 60); // 1 hour
+      Cache::put('cache_instagram', $results, 1); // 1 min
+    }
+    //$instagram = new Instagram();
     //return $instagram->get('_tokyocalling');
-    return view('home', ['items' => $instagram->get('_tokyocalling')]);
+    return view('home', ['items' => $results]);
   }
 }
